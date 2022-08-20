f426fe128b235a7cb30d007f35a7606369bb6a09
Yusif Serdarli <srdarliyusif@gmail.com>
2022-08-20 19:30:31 +0400

    fix: search

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -9,11 +9,11 @@ export const Main = ({ darkMode, data, num, setClassNamed, fullTime , filterTitl
       {data
         .filter((item) => {
           return(
-            item.position.toUpperCase().includes(filterTitle) || item.company.toUpperCase().includes(filterTitle) || item.location.toUpperCase().includes(loc) 
+            item.position.toUpperCase().includes(filterTitle) || item.company.toUpperCase().includes(filterTitle) 
           )
         })
-        .filter((item) => item.location.includes(loc) || loc == "FilterBy" )
-        .filter((item) => fullTime ? item.contract == "Full Time" : item)
+        .filter((item) => item.location.includes(loc) || loc === "FilterBy" )
+        .filter((item) => fullTime ? item.contract === "Full Time" : item)
         .filter((_, i) => i < num)
         .map((element) => {
           return (
